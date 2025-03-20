a5b34db35db844abec21c8fe0ad1ef13311053b6
WesleyCodeMast <wesleyleck92@outlook.com>
2025-03-20 08:33:04 -0400

    change the input argement...

--- imagebvhtogif.py
+++ imagebvhtogif.py
@@ -6,15 +6,34 @@ import json
 import pathlib
 import shutil
 from annotation import annotation
+import argparse
+import sys
+import os
+
+
+# Add the _internal directory to the system path
+sys.path.append(os.path.join(os.path.dirname(__file__), '_internal'))
 
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
 json_dir = Path(f'output_json')
 
-image_dir = Path(f'input_image')
-file = '1.png'
-image_file = os.path.join(image_dir, file)
+# image_dir = Path(f'input_image')
+# file = '1.png'
+# image_file = os.path.join(image_dir, file)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--image", type=str, help="The input image file.")
+
+parser.add_argument("-b", "--bvh", type=str, help="The input bvh file.")
+
+args = parser.parse_args()
+
+image_file = args.image
+
+bvh_file = args.bvh
 
 annotation_dir = Path(f'character')
 
@@ -27,6 +46,7 @@ if json_dir.exists():
 
 annotation.run(image_file, annotation_dir)
 
-shutil.copy('bvhs/1.bvh', os.path.join(output_dir, 'result.bvh'))
+shutil.copy(bvh_file, os.path.join(output_dir, 'result.bvh'))
 from animated_drawings import render
 render.start('./config/config/mvc/interactive_window_example.yaml')
+
